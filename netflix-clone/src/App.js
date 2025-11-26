fe36205620813a74b0188489e28ee2a70e0f2e5a
Anteneh <antetaye@yahoo.com>
2025-11-25 22:31:50 -0600

    Add all untracked files

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
+import Banner from './Components/Banner/Banner';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Row from './Components/Row/Row';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-    <h1> Hello </h1>
+    <Header/>
+    <Home/>
+    <Footer/>
+    <Banner/>
+    <Row/>
+    
+    
     </div>
   );
 }
